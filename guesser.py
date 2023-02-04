8118cee9c258bac92477d195f1bd236e39b36dbf
Cubersinn <124463533+Cubersinn@users.noreply.github.com>
2023-02-04 22:19:28 +0300

    Первая неоконченная версия.

---
+++ guesser.py
@@ -0,0 +1,29 @@
+import random
+
+number = random.randint(1000, 9999)
+print(number)
+
+number = str(number)
+trues = 0
+place = 0
+while True:
+    usernum = input("What number do you think I have made :)? ")
+
+    if usernum == number:
+        print("You win!")
+        break
+    else:
+        try:
+            int(usernum)
+            str(usernum)
+            for i in range(4):
+                if usernum[i] == number[i]:
+                    trues += 1
+                    place += 1
+                elif usernum[i] == number[0] or usernum[i] == number[1] or usernum[i] == number[2] or usernum[i] == number[3]:
+                    trues += 1
+            print("There are {0} digits are right and {1} digits are on their places".format(trues, place))
+            trues = 0
+            place = 0
+        except:
+            print("Enter a valid number!")
